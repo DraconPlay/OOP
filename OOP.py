9f3e91dd720fde5b81c1f6299de94dc5dc1681a8
Alexs <cool.karpuk@mail.ru>
2022-06-12 10:47:51 +0300

    Создали класс Англ. языка

--- OOP.py
+++ OOP.py
@@ -1,1 +1,16 @@
+import string
+
+
 class Alphabet:
+    def __init__(self, lang, letters):
+        self.lang = lang
+        self.letters = letters
+
+    def print(self):
+        print(self.letters)
+
+    def letters_num(self):
+        print(self.letters)
+
+class EngAlphabet:
+    
