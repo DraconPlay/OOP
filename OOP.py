1edede05fa7a6996aa3644f3ff4137b4e3a825de
Alexs <cool.karpuk@mail.ru>
2022-06-12 11:03:30 +0300

    Готово

--- OOP.py
+++ OOP.py
@@ -12,5 +12,30 @@ class Alphabet:
     def letters_num(self):
         print(self.letters)
 
-class EngAlphabet:
-    
+
+class EngAlphabet(Alphabet):
+    def __init__(self):
+        super().__init__('En', string.ascii_uppercase)
+
+    __letters_num = 26
+
+    def is_en_letter(self, letter):
+        if letter.upper() in self.letters:
+            return True
+        return False
+
+    def letters_num(self):
+        return EngAlphabet.__letters_num
+
+    @staticmethod
+    def example():
+        print("Hello world")
+
+
+
+EngAlphabet = EngAlphabet()
+EngAlphabet.print()
+print(EngAlphabet.letters_num())
+print(EngAlphabet.is_en_letter('F'))
+print(EngAlphabet.is_en_letter('Щ'))
+EngAlphabet.example()
